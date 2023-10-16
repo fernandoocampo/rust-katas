e93d540cf606b364d33324d29795ba06e3ba02c5
fernando ocampo <fdocampo@gmail.com>
2023-10-16 21:32:22 +0200

    add find smallest int

--- src/iterations/for_loop.rs
+++ src/iterations/for_loop.rs
@@ -18,8 +18,29 @@ fn repeat(value: char, repeat_number: i8) -> String {
     result
 }
 
+fn find_smallest_int(arr: &[i32]) -> i32 {
+    let mut result: i32 = arr[0];
+
+    for value in arr.into_iter() {
+        if *value < result {
+            result = *value;
+        }
+    }
+
+    result
+}
+
+fn find_smallest_int_smart(arr: &[i32]) -> i32 {
+    match arr.iter().min() {
+        Some(&v) => v,
+        None => 0,
+    }
+}
+
 #[cfg(test)]
 mod sum_tests {
+    use std::collections::HashMap;
+
     use crate::iterations::for_loop;
     #[test]
     fn test_to_upper_case() {
@@ -42,5 +63,37 @@ mod sum_tests {
         let got = for_loop::repeat(char_value, repeat_number);
         // Then
         assert_eq!(want, got);
+    }
+
+    #[test]
+    fn test_find_smallest_int() {
+        // Given
+        let cases = HashMap::from([
+            (&[34, 15, 88, 2], 2),
+            (&[34, -345, -1, 100], -345),
+        ]);
+
+        for (input, want) in cases.into_iter() {
+            // When
+            let got = for_loop::find_smallest_int(input);
+            // Then
+            assert_eq!(want, got);
+        }
+    }
+
+    #[test]
+    fn test_find_smallest_int_smart() {
+        // Given
+        let cases = HashMap::from([
+            (&[34, 15, 88, 2], 2),
+            (&[34, -345, -1, 100], -345),
+        ]);
+
+        for (input, want) in cases.into_iter() {
+            // When
+            let got = for_loop::find_smallest_int_smart(input);
+            // Then
+            assert_eq!(want, got);
+        }
     }
 }
